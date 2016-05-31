f784131d260fa521280bf0b29359f70992a5e820
lesiak <lesiak@outlook.com>
2016-05-31 11:36:49 +0200

    Fixed namespaces

--- JolokiaTest/JolokiaTest/Program.cs
+++ JolokiaTest/JolokiaTest/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Jolokia.Client;
+using Jolokia.Client.Request;
 
 
 namespace JolokiaTest
